f117ddcd96f1ff2bfb88249cac483374f1e0bde6
Sydney <sjbarney99@gmail.com>
2019-06-29 09:51:57 -0600

    should really be hosting

---
+++ build/precache-manifest.abb63b8457e306d4fcc248ed0e452e12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa63407343fedeb1fd5e94f8b892fd10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03ff66a2f5c4cb4b5628",
+    "url": "/static/css/main.6144a32e.chunk.css"
+  },
+  {
+    "revision": "98129a8c00bc8f835750",
+    "url": "/static/js/2.5e9bbed8.chunk.js"
+  },
+  {
+    "revision": "03ff66a2f5c4cb4b5628",
+    "url": "/static/js/main.5eb350f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8dd42409d01faec26cc42e88ed44d740",
+    "url": "/static/media/logo.8dd42409.png"
+  }
+]);
